c10fecb66a55921b71cbd916c1dd36bab72c5147
Oswaldo de la Ossa <oswaldo@joonik.com>
2018-11-02 11:39:19 -0500

    Cambios en vista de verificación de email

--- app/src/main/java/com/oswaldo/tiendakotlin/VerificarEmailActivity.kt
+++ app/src/main/java/com/oswaldo/tiendakotlin/VerificarEmailActivity.kt
@@ -2,6 +2,7 @@ package com.oswaldo.tiendakotlin
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.WindowManager
 
 /**
  * A login screen that offers login via email/password.
@@ -11,6 +12,12 @@ class VerificarEmailActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_verificar_email)
+
+        val window = this.window
+
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = this.resources.getColor(R.color.colorPrimaryDark, null)
     }
 
 }
