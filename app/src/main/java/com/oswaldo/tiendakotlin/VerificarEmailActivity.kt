81e7bba90b3e835a0275b723f597b419ef504cc4
Oswaldo de la Ossa <oswaldo@joonik.com>
2018-11-06 11:37:03 -0500

    Agregado viewmodel de login

--- app/src/main/java/com/oswaldo/tiendakotlin/VerificarEmailActivity.kt
+++ app/src/main/java/com/oswaldo/tiendakotlin/VerificarEmailActivity.kt
@@ -1,8 +1,10 @@
 package com.oswaldo.tiendakotlin
 
+import android.databinding.DataBindingUtil
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
+import com.oswaldo.tiendakotlin.databinding.ActivityVerificarEmailBinding
 
 /**
  * A login screen that offers login via email/password.
@@ -11,10 +13,11 @@ class VerificarEmailActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_verificar_email)
+        val binding = DataBindingUtil.setContentView<ActivityVerificarEmailBinding>(this, R.layout.activity_verificar_email)
+
+
 
         val window = this.window
-
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
         window.statusBarColor = this.resources.getColor(R.color.colorPrimaryDark, null)
---
+++ app/src/main/java/com/oswaldo/tiendakotlin/viewModels/LoginViewModel.kt
@@ -0,0 +1,4 @@
+package com.oswaldo.tiendakotlin.viewModels
+
+class LoginViewModel {
+}
